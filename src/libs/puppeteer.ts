169b79b9534e9462d0bc686e32a9991c4f94d348
Valentyn <xdshkaa2@gmail.com>
2023-06-18 21:09:37 +0300

    desktop-version: chrome instead chronium

--- src/libs/puppeteer.ts
+++ src/libs/puppeteer.ts
@@ -2,4 +2,5 @@ import puppeteer from 'puppeteer';
 
 export const initBrowser = () => puppeteer.launch({
   headless: false,
+  executablePath: 'C:/Program Files/Google/Chrome/Application/chrome.exe',
 });
